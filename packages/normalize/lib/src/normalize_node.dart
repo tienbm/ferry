bbc1c7e4e76afdb28ee9967c1fa442019e26b372
0x1 <tienbm0x1@gmail.com>
2025-05-05 16:51:53 +0700

    Update normalize_node.dart
    fix type 'List<dynamic>' is not a subtype of type 'Map<dynamic, dynamic>'

--- packages/normalize/lib/src/normalize_node.dart
+++ packages/normalize/lib/src/normalize_node.dart
@@ -115,8 +115,12 @@ Object? normalizeNode({
 
     if (dataId != null) existingNormalizedData = config.read(dataId);
 
+    final existingMap = existingNormalizedData is Map
+    ? Map.from(existingNormalizedData)
+    : <String, dynamic>{};
+
     final mergedData = deepMerge(
-      Map.from(existingNormalizedData as Map<dynamic, dynamic>? ?? {}),
+      existingMap,
       dataToMerge,
     );
 
